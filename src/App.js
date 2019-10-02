9cacf115cd653e75c94db909ab23e59387edb7d1
TwentyFive <TwentyFiveSoftware@gmail.com>
2019-10-02 15:26:11 +0200

    Dynamic function containers in function list were added.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './style/index.css';
 
 import TopBar from './components/TopBar';
+import IMG_linearFunction from './assets/linear_function.png';
 
 export default class App extends Component {
+    state = {
+        functions: [
+            {
+                name: 'Lineare Funktionen',
+                image: IMG_linearFunction,
+                grade: 8
+            }
+        ]
+    }
+
     render() {
         return (
             <Router>
@@ -13,6 +24,14 @@ export default class App extends Component {
                     <Route path='/' >
                         <div className="page">
                             <TopBar></TopBar>
+                            <div className="function-list-container">
+                                {(this.state.functions.map((f, index) => (
+                                    <div className="function-list-container__function" key={index}>
+                                        <div className="function-list-container__name">{f.name}</div>
+                                        <div className="function-list-container__image" style={{ backgroundImage: `url(${f.image})` }} alt=" " />
+                                    </div>
+                                )))}
+                            </div>
                         </div>
                     </Route>
 
