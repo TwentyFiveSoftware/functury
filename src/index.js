c9eea7d555b13e0236bdd8a4469aa73ea9e5f24a
TwentyFive <TwentyFiveSoftware@gmail.com>
2019-10-07 21:55:08 +0200

    Supplied PWA requirements.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './App';
+import * as serviceWorker from './scripts/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
