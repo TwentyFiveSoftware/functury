aa6358e7247b8266c27f5b9d0ca5a188cb7cb837
TwentyFive <TwentyFiveSoftware@gmail.com>
2019-10-23 14:31:39 +0200

    Restructured function page layout.

--- src/pages/LinearFunction.js
+++ src/pages/LinearFunction.js
@@ -17,15 +17,28 @@ export default class LinearFunction extends Component {
         return (
             <React.Fragment>
                 <Panel classes={['headline']}>Lineare Funktionen</Panel>
-                <Panel><FunctionPlotter equation={x => this.state.m * x + this.state.t} /></Panel>
+                <div className='table'>
+                    <div className='left'>
+                        <Panel>
+                            <FunctionPlotter equation={x => this.state.m * x + this.state.t} />
+                            <Panel classes={['semibig']}>f(x) = {this.state.m}x {this.state.t < 0 ? '-' : '+'} {Math.abs(this.state.t)}</Panel>
+                        </Panel>
+                    </div>
+                    <div className='right'>
+                        <Panel classes={['big', 'bottomGap']}>f(x) = mx + t</Panel>
+                        <Panel>
+                            <Headline>Steigung (m)</Headline>
+                            <Slider min={-10} max={10} step={0.25} id={'m'} value={this.state.m} change={newValue => this.setState({ m: newValue })} />
+                            <EmptySpace height={30} />
+                            <Headline>y-Achsenabschnitt (t)</Headline>
+                            <Slider min={-10} max={10} step={0.5} id={'t'} value={this.state.t} change={newValue => this.setState({ t: newValue })} />
+                        </Panel>
+                    </div>
+                </div>
                 <Panel>
-                    <Headline>Steigung (m)</Headline>
-                    <Slider min={-10} max={10} step={0.25} id={'m'} value={this.state.m} change={newValue => this.setState({ m: newValue })} />
-                    <EmptySpace height={30} />
-                    <Headline>y-Achsenabschnitt (t)</Headline>
-                    <Slider min={-10} max={10} step={0.5} id={'t'} value={this.state.t} change={newValue => this.setState({ t: newValue })} />
+                    <h1>Nullstellenberechnung</h1>
                 </Panel>
-            </React.Fragment>
-        )
+            </React.Fragment >
+        );
     }
 }
