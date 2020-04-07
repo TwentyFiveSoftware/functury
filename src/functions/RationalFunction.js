0572cb49c2c763ec40b27677935a044a6d9abd7b
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-04-07 15:38:56 +0200

    Added 'Asymptoten' - info field for rational functions.

--- src/functions/RationalFunction.js
+++ src/functions/RationalFunction.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import Fraction from '../components/Fraction';
 import Text from '../components/Text';
-import {EmptySpace, H, InlineSpace, Panel} from '../components/Utils';
+import {EmptySpace, EmptySpaceSmall, H, InlineSpace, Panel} from '../components/Utils';
 import GraphTab from '../components/GraphTab';
+
+import svg_tab2_1 from '../assets/rationalFunction/rationalFunction_tab2_1.svg';
 
 //
 
@@ -43,8 +45,48 @@ function RationalFunctionTab2() {
 
                 <EmptySpace/>
                 <Text>Beispiel:</Text>
-                <EmptySpace small={true}/>
+                <EmptySpaceSmall/>
                 <Text>f(x) = <Fraction top={'1'} bottom={'x + 2'}/> <InlineSpace/>D = &#8474; \ {'{-2}'}</Text>
+            </Panel>
+
+            {/*<Panel largePadding={true}>*/}
+            {/*    <Text config={{headline: true}}>Symmetrie</Text>*/}
+            {/*    /!*<Text>Gebrochenrationale Funktionen sind punktsymmetrisch zum Ursprung (0 / 0), wenn jeder Punkt am .</Text>*!/*/}
+
+            {/*    <Text>[INFO]:</Text>*/}
+            {/*    <Text>Berechnen, ob eine Punktsymmetrie vorliegt:</Text>*/}
+            {/*    /!*<EmptySpaceSmall/>*!/*/}
+            {/*    <Equation equation={[['-f(x)', 'f(-x)', '']]}/>*/}
+            {/*</Panel>*/}
+
+            <Panel largePadding={true}>
+                <Text config={{headline: true}}>Asymptoten</Text>
+                <Text>Gebrochenratonale Funktionen nähern sich im unendlichen stets einer parallelen Gerade zur x-Achse an, der <b>waagerechten Asymptote</b>.</Text>
+                <EmptySpaceSmall/>
+                <Text>Da die gebrochenrationalen Funktionen meist Definitionslücken besitzen, entstehen an diesen x-Werten sogenannte <b>senkrechte Asymptoten</b>.</Text>
+                <EmptySpace/>
+
+                <Text>Beispiel:</Text>
+
+                <table>
+                    <tbody>
+                    <tr>
+                        <td><img className={'info-svg'} src={svg_tab2_1} alt={''}/></td>
+                        <td>
+                            <Text>f(x) = <Fraction top={'1'} bottom={'x - 2'}/> + 1 <InlineSpace/>D = &#8474; \ {'{2}'}</Text>
+                            <EmptySpace/>
+
+                            <Text><span style={{color: '#4EAEEE'}}>waagerechte Asymptote:</span></Text>
+                            <Text>x = 1</Text>
+                            <EmptySpaceSmall/>
+                            <Text><span style={{color: '#FE6055'}}>senkrechte Asymptote:</span></Text>
+                            <Text>y = 2</Text>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+
             </Panel>
 
         </div>
